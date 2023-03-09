e3c440aa28d06086e7ca09f7eca35b569503165f
stephanie <stephanie.barreto@hotmail.com>
2023-03-09 10:25:18 -0300

    adjustment in flutter modular

--- lib/modules/shared/app_widget.dart
+++ lib/modules/shared/app_widget.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-
-import 'constantes/app_colors.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Find Weather',
-      theme: ThemeData(
-        primarySwatch: AppColors.primary,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      // locale: DevicePreview.locale(context), // Add the locale here
+      // builder: DevicePreview.appBuilder,
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
     );
   }
 }
